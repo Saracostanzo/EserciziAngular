d2d5145e739ae000c2736c89453ae24e9270e828
Sara <costanzosara96@gmail.com>
2022-03-28 15:57:55 +0200

    correzionees10

--- src/app/app.service.module.ts
+++ src/app/app.service.module.ts
@@ -1,19 +1,21 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import {Observable, Subject } from "rxjs";
 
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class AppService {
-counter: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+counter: Subject<number> = new Subject<number>();
  public counter$=this.counter.asObservable()
 
 getAll():Observable<number>{
 return this.counter$
 }
   remove( value: number) {
-    console.log(this.counter.value)
+    console.log(this.counter)
   console.log(value)
     if(this.counter.value >= value){
     this.counter.next(this.counter.value - value)
